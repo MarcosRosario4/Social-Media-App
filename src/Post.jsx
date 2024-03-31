05f5c39c02b98be2079506e4929cb5ff1dc31690
Macbook <macbook@Catherines-MBP.lan>
2024-03-31 14:54:15 -0400

    Comment Component update

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Comment from './Comment';
 
 function Post(){
 
@@ -9,6 +10,7 @@ return(
 <p>This is a test post we'll edit later!</p>
 <p>Likes: 30</p>
 <p>Comments:</p>
+<Comment />
 </div>
 
 )
