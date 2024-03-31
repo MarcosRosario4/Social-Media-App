2eba814d8eb102f897a33ed64fd76b61955d93f1
Macbook <macbook@Catherines-MBP.lan>
2024-03-31 14:49:49 -0400

    Created Post Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
 import React from 'react';
+import Post from './Post';
 
 function App(){
 
-return(
-
-<div>
-<h1>Fakebook!</h1>
-{/* CreatePostForm */}
-{/* Feed */}
-{/* Post */}
-</div>
-)
-}
-export default App
+  return(
+  
+  <div>
+  <h1>Fakebook!</h1>
+  
+  {/* CreatePostForm */}
+  
+  {/* Feed */}
+  
+  <Post />
+  
+  </div>
+  
+  )
+  }
+  export default App
