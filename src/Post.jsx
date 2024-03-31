900d2bf6352b2f82f726605209ee6856ce6f8bbe
Macbook <macbook@Catherines-MBP.lan>
2024-03-31 15:18:41 -0400

    Counting Likes

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,16 +3,23 @@ import Comment from './Comment';
 
 function Post(){
 
-return(
+    let likes = 30;
 
-<div>
-<h3>Test Post</h3>
-<p>This is a test post we'll edit later!</p>
-<p>Likes: 30</p>
-<p>Comments:</p>
-<Comment content="This is a test comment!" />
-<Comment content="This is another test comment!" />
-<Comment content="This is yet another test comment!" />
+    function handleLike(){
+        likes = likes + 1;
+    }
+
+  return(
+
+    <div>
+        <h3>Test Post</h3>
+        <p>This is a test post we'll edit later!</p>
+        <p>Likes: 30</p>
+        <button>Like</button>
+        <p>Comments:</p>
+        <Comment content="This is a test comment!" />
+        <Comment content="This is another test comment!" />
+        <Comment content="This is yet another test comment!" />
 
 </div>
 
